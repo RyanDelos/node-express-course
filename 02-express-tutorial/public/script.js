b545054dde0c8b2b3c90884fe28f338be429924d
Ryan Delos Santos <delossantosrryan@gmail.com>
2024-03-21 13:54:18 -0700

    refactor code

--- 02-express-tutorial/public/script.js
+++ 02-express-tutorial/public/script.js
@@ -7,16 +7,17 @@ const fetchProducts = async () => {
   try {
     const response = await fetch('api/v1/products');
     const data = await response.json();
-    return JSON.stringify(data, null, 2);
+    dataContainer.innerText = JSON.stringify(data, null, 2);
   } catch (error) {
-    console.log(error);
+    dataContainer.innerText = String(error);
   }
 };
 
-prodBtn.addEventListener('click', (e) => {
-  console.log('clicked');
-  fetchProducts().then((result) => {
-    console.log(result);
-    dataContainer.innerText = result;
+if (prodBtn) {
+  prodBtn.addEventListener('click', (e) => {
+    console.log('clicked');
+    fetchProducts();
   });
-});
+} else {
+  console.error('unable to locate product button');
+}
