8e4577165072b767780a71e4dafc88fc052a2c99
Ryan Delos Santos <delossantosrryan@gmail.com>
2024-03-30 11:27:27 -0700

    add modified files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,64 +1,21 @@
 const express = require('express');
 const app = express();
 
-const { products } = require('./data');
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
-app.use(express.static('./public'));
+//  static assets
+app.use(express.static('./methods-public'));
 
-app.get('/api/v1/test', (req, res) => {
-  res.json({ message: 'it worked!' });
-});
+// Parse form data -> handles the post data
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/v1/products', (req, res) => {
-  // console.log(products);
-  res.json(products);
-});
+// parse json
+app.use(express.json());
 
-app.get('/api/v1/products/:productID', (req, res) => {
-  const idToFind = parseInt(req.params.productID);
+app.use('/api/people', people);
+app.use('/login', auth);
 
-  const product = products.find((p) => p.id === idToFind);
-
-  if (!product) {
-    return res.status(404).send('That product was not found :(');
-  }
-  return res.json(product);
-});
-
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, parseInt(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-
-  res.status(200).json(sortedProducts);
-});
-
-app.get('/api/v1/products/below/:price', (req, res) => {
-  const maxPrice = parseFloat(req.params.price);
-  const productPrice = products.find((p) => p.price <= maxPrice);
-
-  return res.json(productPrice);
-});
-
-app.post('/', (req, res) => {
-  res.send('posted :)');
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found');
-});
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000...');
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....');
 });
